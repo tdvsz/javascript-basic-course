875cb0103b03e17d2ebbc010e379e4c3db4dff56
Максим <avtuhmaxim@yandex.by>
2024-01-31 18:24:02 +0300

    Домашняя работа 5

---
+++ 5-languages/app.js
@@ -0,0 +1,18 @@
+const userLanguage = prompt('Enter youre language:');
+switch (userLanguage) {
+    case 'en':
+        console.log('Good afternoon!');
+        break;
+    case 'de':
+        console.log('Gutten tag!');
+        break;
+    case 'ru':
+        console.log('Добрый день!');
+        break;
+    case 'bel':
+        console.log('Добры дзень!');
+        break;
+    case 'cht':
+        console.log('午安');
+        break;
+}
