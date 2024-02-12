f9089047154998f05a1c7535126333cf8e4f238b
Максим <avtuhmaxim@yandex.by>
2024-02-12 22:31:06 +0300

    Исправил
    Объявил переменную, переписал функцию

--- 8-crypto/crypto.js
+++ 8-crypto/crypto.js
@@ -1,15 +1,11 @@
 let word = 'password';
 let word2 = 'password';
 const encoderWord = crypto(word);
-// console.log(check(encoderWord, word2));
-
-
-
 
 function crypto(word) {
     let lenWord = (word.length) / 2;
     let partWord1 = word.split('');
-    partWord2 = partWord1.splice(lenWord);
+    let partWord2 = partWord1.splice(lenWord);
     partWord1.reverse(); partWord2.reverse();
     let encoderWord = partWord1.join('') + partWord2.join('');
     return encoderWord;
@@ -18,10 +14,7 @@ function crypto(word) {
 console.log(crypto(word));
 
 function check(encoderWord, word2) {
-    let reverseWord = crypto(encoderWord);
-    if (reverseWord === word2) {
-        return true;
-    }
-    else return false;
+    return crypto(word2) === encoderWord;
 }
+
 console.log(check(encoderWord, word2));
