5b029f569182178e51447c130bf3f98c151d9c42
Максим <avtuhmaxim@yandex.by>
2024-02-18 14:31:15 +0300

    Исправил и добавил

--- 7-converter/converter.js
+++ 7-converter/converter.js
@@ -1,10 +1,10 @@
-console.log(converter(1000, 'RUB', 'BYN'));
+console.log(converter(1000, 'USD', 'RUB'));
 
 function converter(amountOfFunds, currencyOfFunds, targetCurrency) {
-    const conversionRate = {USD : 90, EUR : 100, BYN : 28, CNY : 12,};
+    const conversionRate = {'RUB': {'EUR': 0.010, 'USD': 0.011}, 'EUR': {'USD': 1.08, 'RUB': 99.67}, 'USD': {'EUR': 0.93, 'RUB': 92.50}};
 
-    if (currencyOfFunds === 'RUB' && conversionRate[targetCurrency]) {
-        const newCurrency = amountOfFunds / conversionRate[targetCurrency];
+    if (conversionRate[currencyOfFunds] && conversionRate[currencyOfFunds][targetCurrency]) {
+        const newCurrency = amountOfFunds * conversionRate[currencyOfFunds][targetCurrency];
         return newCurrency;
     }
     else {
