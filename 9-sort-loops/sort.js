26ed95df400b747b4c9e9d3679e6bd4db757356d
Максим <avtuhmaxim@yandex.by>
2024-02-20 13:01:54 +0300

    Клонирование массива

--- 9-sort-loops/sort.js
+++ 9-sort-loops/sort.js
@@ -1,16 +1,19 @@
 let arr = [1, 40, -5, 10, 0];
 
-console.log(sort(arr))
+let newArr = [...arr] // клонируем массив
 
-function sort(arr) {
-    for (let x = 0; x < arr.length; x++) {
-        for (let i = x; i < arr.length; i++) {
-            if (arr[x] > arr[i]) {
-                let t = arr[x];
-                arr[x] = arr[i];
-                arr[i] = t;
+function sort(newArr) {
+    for (let x = 0; x < newArr.length; x++) {
+        for (let i = x; i < newArr.length; i++) {
+            if (newArr[x] > newArr[i]) {
+                let t = newArr[x];
+                newArr[x] = newArr[i];
+                newArr[i] = t;
             }
         }
     }
-    return arr;
+    return newArr;
 }
+
+console.log(arr);
+console.log(sort(newArr));
